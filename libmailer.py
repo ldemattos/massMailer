8b63cc83b09d37ee97b7104141eba31099e0becb
Leonardo M. N. de Mattos <l@mattos.eng.br>
2017-06-01 16:01:31 -0300

    Disconnection and localhost connection

--- libmailer.py
+++ libmailer.py
@@ -24,10 +24,22 @@ import smtplib
 import sys
 
 def mailer(parser,msg,fromf,subf,tof,fp_log):
+
 	try:
-		smtpObj = smtplib.SMTP_SSL(parser.get('smtp','server')+':'+parser.get('smtp','port'),timeout=float(parser.get('smtp','timeout')))
-		smtpObj.login(parser.get('smtp','login'),parser.get('smtp','password'))
-		smtpObj.sendmail(fromf, tof, msg)
-		fp_log.write("OK!\n")
+		if parser.get('smtp','server') == 'localhost':
+			smtpObj = smtplib.SMTP('localhost')
+			smtpObj.debuglevel(2)
+			smtpObj.sendmail(fromf, tof, msg)
+			smtpObj.quit()
+			fp_log.write("OK!\n")
+		else:
+			smtpObj = smtplib.SMTP_SSL(parser.get('smtp','server')+':'+parser.get('smtp','port'),timeout=float(parser.get('smtp','timeout')))
+			smtpObj.debuglevel(2)
+			smtpObj.login(parser.get('smtp','login'),parser.get('smtp','password'))
+			smtpObj.sendmail(fromf, tof, msg)
+			smtpObj.quit()
+			fp_log.write("OK!\n")
 	except Exception:
+		print sys.exc_info()
+		smtpObj.quit()
 		fp_log.write("FAIL: %s\n"%(sys.exc_info()[0]))
